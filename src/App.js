361b6717878a838448758ae9d76078df144b72f6
Skandh Gupta <skandhoff@gmail.com>
2019-11-14 21:50:01 +0530

    Used state property

--- src/App.js
+++ src/App.js
@@ -3,17 +3,29 @@ import React, { Component } from 'react';
 import './App.css';
 import Person from './Company/Company';
 class App extends Component {
+  state = {
+    character:[
+      {name : 'Ross Geller',tagline : "I'm fine."},
+      {name : 'Monika Geller',tagline : "I'm drunk."},
+      {name : 'Joey Tribbiani',tagline : "Joey doesn't share food."},
+      {name: 'Chandler Bing',tagline : "Can i interest you in sarcastic comment."},
+      {name: 'Phoebe Buffay', tagline : "My mother killed herself."},
+      {name : 'Rachel Green', tagline : "We were not on a break."}
+    ]
+  }
+
   render() {
     return (
       <div className="App">
         <h1>React Application</h1>
         <h2>Welcome to the Friends</h2>
-        <Person name = 'Ross Geller' tagline = "I'm fine."/>
-        <Person name = 'Monika Geller' tagline = "I'm drunk."/>
-        <Person name = 'Joey Tribbiani' tagline = "Joey doesn't share food."/>
-        <Person name = 'Chandler Bing' tagline = "Can i interest you in sarcastic comment."/>
-        <Person name = 'Phoebe Buffay' tagline = "My mother killed herself."/>
-        <Person name = 'Rachel Green' tagline = "We were not on a break."/>
+        <button>Switch Character</button>
+        <Person name = { this.state.character[0].name } tagline = { this.state.character[0].tagline } />
+        <Person name = { this.state.character[1].name } tagline = { this.state.character[1].tagline } />
+        <Person name = { this.state.character[2].name } tagline = { this.state.character[2].tagline } />
+        <Person name = { this.state.character[3].name } tagline = { this.state.character[3].tagline } />
+        <Person name = { this.state.character[4].name } tagline = { this.state.character[4].tagline } />
+        <Person name = { this.state.character[5].name } tagline = { this.state.character[5].tagline } />
       </div>
     );
   }
