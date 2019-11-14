589d43bd7a8136015530ebe4e4eb02aab83f4173
Skandh Gupta <skandhoff@gmail.com>
2019-11-14 08:05:54 +0530

    Removed unsused Component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Company/Comapny.js';
-import Manager from './Managers/Manager';
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>React Application</h1>
         <Person />
-        <Manager />
       </div>
     );
   }
