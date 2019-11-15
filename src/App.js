d2734e98a72f39e70093d113b775f27ce4d7b2b9
Skandh Gupta <skandhoff@gmail.com>
2019-11-15 07:52:43 +0530

    Changed Props and state using State and event handling

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ class App extends Component {
       <div className="App">
         <h1>React Application</h1>
         <h2>Welcome to the Friends</h2>
-        <button>Switch Character</button>
+        <button onClick = {this.CharacterSwitchHandler}>Switch Character</button>
         <Person name = { this.state.character[0].name } tagline = { this.state.character[0].tagline } />
         <Person name = { this.state.character[1].name } tagline = { this.state.character[1].tagline } />
         <Person name = { this.state.character[2].name } tagline = { this.state.character[2].tagline } />
@@ -29,6 +29,20 @@ class App extends Component {
       </div>
     );
   }
+
+  CharacterSwitchHandler = () =>{
+    this.setState({
+      character:[
+        {name : 'Ross Geller', tagline : "We were on a break."},
+        {name : 'Monika Geller',tagline : "I'm drunk."},
+        {name : 'Joey Tribbiani',tagline : "Joey doesn't share food."},
+        {name: 'Chandler Bing',tagline : "Can i interest you in sarcastic comment."},
+        {name: 'Phoebe Buffay', tagline : "My mother killed herself."},
+        {name : 'Rachel Green', tagline : "We were not on a break."} 
+      ]}
+    )
+  }
 }
+
 
 export default App;
