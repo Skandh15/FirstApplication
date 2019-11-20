58736d2ba3ef1ce82c24d94ae7fbc9542c729019
Skandh Gupta <skandhoff@gmail.com>
2019-11-20 09:22:46 +0530

    Formatted code

--- src/Company/Company.js
+++ src/Company/Company.js
@@ -1,9 +1,8 @@
 import React from 'react';
 const company = (props) => {
     return (
-        <div>
-            
-<h3>Hi, I am { props.name }. <br></br> { props.tagline }</h3>
+        <div>       
+            <h3>Hi, I am { props.name }. <br></br> { props.tagline }</h3>
         </div>
         )
 };
