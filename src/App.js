8ce6f0073799612cb350bd3f691825e4e45f54c5
Skandh Gupta <skandhoff@gmail.com>
2019-11-24 08:22:25 +0530

    Added event handling to state component and functional component

--- src/App.js
+++ src/App.js
@@ -22,27 +22,41 @@ class App extends Component {
         <p><strong>The difference between state and props is that state works in the same js while in props we have to pass props to another component.</strong></p>
         <button onClick = {this.CharacterSwitchHandler.bind(this,'It\'s not that common, it\'s not that easy and it doesn\'t happen to every guy.')}>Switch Character</button>
         <h5>Hi, I am { this.state.character[0].name }. <br></br>  { this.state.character[0].tagline } </h5>
-        <h5>Hi, I am { this.state.character[1].name }. <br></br>  { this.state.character[1].tagline } </h5>
-        <h5>Hi, I am { this.state.character[2].name }. <br></br>  { this.state.character[2].tagline } </h5>
+        <h5>Hi, I am { this.state.character[1].name } { this.CharacterSwitchHandler }. <br></br>  { this.state.character[1].tagline } </h5>
+        <input type = 'text' onChange = {this.CharacterSwitchHandler}></input>
+        <h5>Hi, I am { this.state.character[2].name }{ this.CharacterSwitchHandler }. <br></br>  { this.state.character[2].tagline } </h5>
         <h5>Hi, I am { this.state.character[3].name }. <br></br>  { this.state.character[3].tagline } </h5> 
         <h5>Hi, I am { this.state.character[4].name }. <br></br>  { this.state.character[4].tagline } </h5>
         <h5>Hi, I am { this.state.character[5].name }. <br></br>  { this.state.character[5].tagline } </h5>
         <h1>--------------------------------------------------------------------------------</h1>
         <h2>Use of Props </h2>
         <Person name = 'Janice' tagline = 'Oh My God'></Person>
+        <Person name = { this.state.character[1].name } changed = {this.NameChangehandler} tagline = { this.state.character[1].tagline }></Person>
       </div>
     );
   }
 
-  CharacterSwitchHandler = (newTagline) =>{
+  CharacterSwitchHandler = (Newevent) =>{
     this.setState({
       character:[
         {name : 'Ross Geller', tagline : "We were on a break."},
-        {name : 'Monika Geller',tagline : "I'm drunk."},
+        {name : 'Monika' ,tagline: "I'm drunk."},
+        {name : 'Joey Tribbiani',tagline : "Joey doesn't share food."},
+        {name: Newevent.target.value,tagline : "Can i interest you in sarcastic comment."},
+        {name: 'Phoebe Buffay', tagline : "My mother killed herself."},
+        {name : 'Rachel Green', tagline : 'newTagline'} 
+      ]}
+    )
+  }
+  NameChangehandler = (event) =>{
+    this.setState({
+      character:[
+        {name : 'Ross Geller', tagline : "We were on a break."},
+        {name : event.target.value,tagline : "I'm drunk."},
         {name : 'Joey Tribbiani',tagline : "Joey doesn't share food."},
         {name: 'Chandler Bing',tagline : "Can i interest you in sarcastic comment."},
         {name: 'Phoebe Buffay', tagline : "My mother killed herself."},
-        {name : 'Rachel Green', tagline : newTagline} 
+        {name : 'Rachel Green', tagline : 'this.newTagline'} 
       ]}
     )
   }
--- src/Company/Company.js
+++ src/Company/Company.js
@@ -1,8 +1,11 @@
+/* eslint-disable */
+
 import React from 'react';
 const company = (props) => {
     return (
         <div>       
             <h3>Hi, I am { props.name }. <br></br> { props.tagline }</h3>
+            <input type = 'text' onChange = {props.changed}></input>
         </div>
         )
 };
